a238216d6d3e016ee9a8fddf4c09612a4d1e7a22
helme__c <claire.helme_guizon@epita.fr>
2019-10-16 14:46:42 +0200

    [MODEL] Started bias as matrix and LSA documentation

--- README.md
+++ README.md
@@ -2,10 +2,8 @@ This is a end of studies' project, realized with Exensa.
 
 # TODO
 
-- [ ] Wtf is there a linear kernel applied to tfidf matrix ?
-
 - [ ] Burutal tokenizer to 10 - meilleur résultat avec 2 askip
-- [ ] html + meta open graph
+- [x] html + meta open graph
 - [ ] order of sentences in document
 - [ ] RageRank Cython
 
--- scores/get_model_with_higher_score.py
+++ scores/get_model_with_higher_score.py
@@ -6,11 +6,12 @@ import pandas as pd
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Error: Expecting one argument, the csv file to analyse.")
+        exit(1)
 
     fname = sys.argv[1]
     res = pd.read_csv(fname, index_col=0)
 
-    max_row_id = res["avg_rl_r"].idxmax()
+    max_row_id = res[["avg_rl_r", "sampling_size"]].idxmax()
     row = res.iloc[max_row_id]
-    print(row["summarizer_name"])
+    print(row["summarizer_name"].values)
     print(row)
